db95c83ddc5f66a8a158322111a5f78801ee2062
Jeremy Kepf <jeremy1975@gmail.com>
2020-01-27 21:40:32 -0500

    media page tweaks

--- src/views/MediaPage.vue
+++ src/views/MediaPage.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="media">
+  <v-container fluid>
     <h1 class="text-center mb-3">This is a media page</h1>
-    <v-row>
-      <v-card class="mx-auto" max-width="560" outlined>
+    <v-row justify="center" align="center">
+      <v-card class="mx-auto" outlined>
+        <v-card-title>Leaves</v-card-title>
         <v-list-item three-line>
-          <v-list-item-content contain>
+          <v-list-item-content class="iframe-container">
             <iframe
-              width="560"
-              height="315"
+              width="100%"
+              height="100%"
               src="https://www.youtube.com/embed/YgBGb73WmrY"
               frameborder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope;
+            picture-in-picture"
               allowfullscreen
-            ></iframe>
+            >
+            </iframe>
           </v-list-item-content>
         </v-list-item>
       </v-card>
-      <v-card class="mx-auto" max-width="560" outlined>
+      <v-card class="mx-auto" outlined>
+        <v-card-title>Meester Jubenesah</v-card-title>
         <v-list-item three-line>
-          <v-list-item-content contain>
+          <v-list-item-content class="iframe-container">
             <iframe
-              width="560"
-              height="315"
+              width="100%"
+              height="100%"
               src="https://www.youtube.com/embed/L_Rzy9W1sdg"
               frameborder="0"
               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
@@ -31,5 +35,10 @@
         </v-list-item>
       </v-card>
     </v-row>
-  </div>
+  </v-container>
 </template>
+<style>
+.iframe-container {
+  overflow: hidden;
+}
+</style>
