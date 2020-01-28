a7457ee113704473602fde74e21ea49e29573003
Jeremy Kepf <jeremy1975@gmail.com>
2020-01-27 20:39:47 -0500

    releases page start

--- src/views/ReleasesPage.vue
+++ src/views/ReleasesPage.vue
@@ -1,5 +1,55 @@
 <template>
   <div class="releases">
-    <h1>This is a releases page</h1>
+    <h1 class="text-center">This is a releases page</h1>
+    <v-row>
+      <v-card class="mx-auto" max-width="500" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <iframe
+              src="https://open.spotify.com/embed/track/2Wi8GGOdn8oIzUK5AYOo86"
+              width="300"
+              height="380"
+              frameborder="0"
+              allowtransparency="true"
+              allow="encrypted-media"
+            ></iframe>
+          </v-list-item-content>
+
+          <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+        </v-list-item>
+      </v-card>
+      <v-card class="mx-auto" max-width="500" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <iframe
+              src="https://open.spotify.com/embed/track/6LSQvuoYpPZyR9dtpMrhes"
+              width="300"
+              height="380"
+              frameborder="0"
+              allowtransparency="true"
+              allow="encrypted-media"
+            ></iframe>
+          </v-list-item-content>
+
+          <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+        </v-list-item>
+      </v-card>
+      <v-card class="mx-auto" max-width="500" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <iframe
+              src="https://open.spotify.com/embed/track/28DTVY7HH6TDXX9whXGIML"
+              width="300"
+              height="380"
+              frameborder="0"
+              allowtransparency="true"
+              allow="encrypted-media"
+            ></iframe>
+          </v-list-item-content>
+
+          <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+        </v-list-item>
+      </v-card>
+    </v-row>
   </div>
 </template>
